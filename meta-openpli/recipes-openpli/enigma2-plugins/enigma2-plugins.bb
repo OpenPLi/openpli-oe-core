OpenPLi/openpli-oe-core

commit f98903399ef0d1df0d5b45087830c8fde40a43da

enigma2-plugins.bb: Solve the [build-deps] warnings
Make it so that OE can detect RDEPENDS at parse time. For this, all
plugins with external dependencies must be listed explicitly rather
than by mask.

meta-openpli/recipes-openpli/enigma2-plugins/enigma2-plugins.bb
@@ -13,6 +13,9 @@ PACKAGES += "\
 	enigma2-plugin-extensions-mosaic \
 	enigma2-plugin-extensions-fancontrol2 \
 	enigma2-plugin-extensions-bonjour \
+	enigma2-plugin-extensions-transmission \
+	enigma2-plugin-systemplugins-systemtime \
+	enigma2-plugin-systemplugins-blindscan \
 	"
 RDEPENDS_enigma2-plugin-extensions-mosaic = "aio-grab"
 RDEPENDS_enigma2-plugin-extensions-fancontrol2 = "smartmontools hdparm"
@@ -58,7 +61,7 @@ FILES_enigma2-plugin-extensions-netcaster += "${sysconfdir}/NETcaster.conf"
 CONFFILES_enigma2-plugin-extensions-netcaster += "${sysconfdir}/NETcaster.conf"
 
 FILES_${PN}-meta = "${datadir}/meta"
-PACKAGES += "${PN}-meta"
+PACKAGES += "${PN}-meta ${PN}-build-dependencies"
 
 inherit autotools-brokensep
 

commit 97bce4351523372f4193ecadf1125815a7fe197d

enigma2-plugins.bb: Don't tell OE about blindscan
The blindscan dependencies are borked, and cause multiple providers
to be built for "virtual/blindscan-dvbs". Until that's sorted out,
don't let OE detect the dependency.

Fixes: f98903399ef0d1df0d5b45087830c8fde40a43da
    enigma2-plugins.bb: Solve the [build-deps] warnings

meta-openpli/recipes-openpli/enigma2-plugins/enigma2-plugins.bb
@@ -15,7 +15,6 @@ PACKAGES += "\
 	enigma2-plugin-extensions-bonjour \
 	enigma2-plugin-extensions-transmission \
 	enigma2-plugin-systemplugins-systemtime \
-	enigma2-plugin-systemplugins-blindscan \
 	"
 RDEPENDS_enigma2-plugin-extensions-mosaic = "aio-grab"
 RDEPENDS_enigma2-plugin-extensions-fancontrol2 = "smartmontools hdparm"