df3a86880588d132b9234e6db4d7fe84c2345bde
littlesat <littlesat99@yahoo.com>
2016-11-05 11:44:00 +0100

    Ensure the /etc/autoinstall file is generated always during flashing

--- meta-openpli/recipes-openpli/enigma2/settings-autorestore/autoinstall.sh
+++ meta-openpli/recipes-openpli/enigma2/settings-autorestore/autoinstall.sh
@@ -1,10 +1,8 @@
 #! /bin/sh
-# This script is run once when your box boots for the first time.
 # It installs the things from /hdd/backup/autoinstall
 # or from wherever the settings were restored
 
 BACKUPDIR=/media/hdd
-INSTALLED=/etc/installed
 LOGFILE=/var/log/autoinstall.log
 MACADDR=`cat /sys/class/net/eth0/address | cut -b 1,2,4,5,7,8,10,11,13,14,16,17`
 
@@ -39,9 +37,6 @@ else
 fi
 
 IPKG=/usr/bin/opkg
-
-${IPKG} list_installed | cut -d ' ' -f 1 > ${INSTALLED}
-chmod 444 ${INSTALLED}
 
 # when available, bind the console during autoinstall
 [ -f /sys/class/vtconsole/vtcon1/bind ] && echo 1 > /sys/class/vtconsole/vtcon1/bind
