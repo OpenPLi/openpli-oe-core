c1a751709234218ce4a4c1eeed1ca9b90f9957a8
Dima73 <Dima-73@inbox.lv>
2016-12-14 20:42:37 +0200

    [mdev-mount.sh] simplify code part 2 (thanks Taapat)

--- meta-openpli/recipes-core/busybox/busybox/mdev-mount.sh
+++ meta-openpli/recipes-core/busybox/busybox/mdev-mount.sh
@@ -15,9 +15,8 @@ case "$ACTION" in
 			# Already mounted
 			exit 0
 		fi
-		# blacklisted uno4k/solo4k/ultimo4k/hd51 internal mmc
-		DEVCHECK=`expr substr $MDEV 1 7`
-		if [ $DEVCHECK == "mmcblk0" ] && [ -f /etc/mdev/mmc-blacklist ]; then
+		# blacklisted internal mmc
+		if [ -e /dev/root ] && [ $MDEV == $(readlink /dev/root) ] ; then
 			exit 0
 		fi
 		DEVBASE=`expr substr $MDEV 1 3`
