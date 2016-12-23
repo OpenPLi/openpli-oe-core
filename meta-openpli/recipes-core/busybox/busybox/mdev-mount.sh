OpenPLi/openpli-oe-core

commit 41f7dd28cb80cfa5c530501d5281fd7bd0871322

[mdev-mount] add detection internal HDD format HD51
/sys/block/sda :
/sys/devices/platform/rdb/f045a000.sata/ata1/host0/target0:0:0/0:0:0:0

meta-openpli/recipes-core/busybox/busybox/mdev-mount.sh
@@ -58,7 +58,7 @@ case "$ACTION" in
 			# no fstab entry, use automatic mountpoint
 			if [ -z "${LABEL}" ] ; then
 				REMOVABLE=`cat /sys/block/$DEVBASE/removable`
-				readlink -fn /sys/block/$DEVBASE/device | grep -qs 'pci\|ahci'
+				readlink -fn /sys/block/$DEVBASE/device | grep -qs 'pci\|ahci|sata'
 				EXTERNAL=$?
 				if [ "${REMOVABLE}" -eq "0" -a $EXTERNAL -eq 0 ] ; then
 					# mount the first non-removable internal device on /media/hdd

commit 6745ae45476ad4ba39336c6f58031dcf876aeb15

Fix typo in harddisk detection, caused harddisk to mount as "usb" instead
Missing "\", example:
~# echo hallo | grep 'al|lo'
~# echo hallo | grep 'al\|lo'
hallo
~#

Fixes: 41f7dd28cb80 "[mdev-mount] add detection internal HDD format HD51"

meta-openpli/recipes-core/busybox/busybox/mdev-mount.sh
@@ -58,7 +58,7 @@ case "$ACTION" in
 			# no fstab entry, use automatic mountpoint
 			if [ -z "${LABEL}" ] ; then
 				REMOVABLE=`cat /sys/block/$DEVBASE/removable`
-				readlink -fn /sys/block/$DEVBASE/device | grep -qs 'pci\|ahci|sata'
+				readlink -fn /sys/block/$DEVBASE/device | grep -qs 'pci\|ahci\|sata'
 				EXTERNAL=$?
 				if [ "${REMOVABLE}" -eq "0" -a $EXTERNAL -eq 0 ] ; then
 					# mount the first non-removable internal device on /media/hdd