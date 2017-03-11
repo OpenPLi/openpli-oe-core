c521a28891b9eb4d219bcfac2b350e45e3bfd955
ims <ims21@users.sourceforge.net>
2017-03-11 20:06:53 +0100

    set logfile autoinstall.log to /home/root
    (cherry picked from commit 77fb0cced49e1720c2631771743bc80ffa79f601)
    Signed-off-by: Erik Slagter <erik@openpli.org>
    
    Conflicts:
    	meta-openpli/recipes-openpli/enigma2/settings-autorestore/autoinstall.sh

--- meta-openpli/recipes-openpli/enigma2/settings-autorestore/autoinstall.sh
+++ meta-openpli/recipes-openpli/enigma2/settings-autorestore/autoinstall.sh
@@ -5,7 +5,7 @@
 
 BACKUPDIR=/media/hdd
 INSTALLED=/etc/installed
-LOGFILE=/var/log/autoinstall.log
+LOGFILE=/home/root/autoinstall.log
 MACADDR=`cat /sys/class/net/eth0/address | cut -b 1,2,4,5,7,8,10,11,13,14,16,17`
 
 if [ -f /tmp/backupdir ]
@@ -28,7 +28,6 @@ else
 			fi
 		fi
 	done
-	LOGFILE=${BACKUPDIR}/backup/autoinstall.log
 fi
 
 if [ -f ${BACKUPDIR}/backup/autoinstall${MACADDR} ]
