a497252d5599d07869bb8f46854898b60b7e6d61
Harro Verton <wanwizard@wanwizard.eu>
2017-03-19 22:15:46 +0100

    removed old code added after incorrect merge conflict fixes

--- meta-openpli/recipes-core/busybox/busybox/mdev-mount.sh
+++ meta-openpli/recipes-core/busybox/busybox/mdev-mount.sh
@@ -58,43 +58,35 @@ case "$ACTION" in
 			# no fstab entry, use automatic mountpoint
 			if [ -z "${LABEL}" ] ; then
 				REMOVABLE=`cat /sys/block/$DEVBASE/removable`
-				if [ "$DRIVER" = "mmcblk" ]; then
-					if [ "${REMOVABLE}" -eq "0" ]; then
-						DEVICETYPE="mmc"
-					else
-						DEVICETYPE="SD-card"
-					fi
+				readlink -fn /sys/block/$DEVBASE/device | grep -qs 'pci\|ahci\|sata'
+				EXTERNAL=$?
+				if [ "${REMOVABLE}" -eq "0" -a $EXTERNAL -eq 0 ] ; then
+					# mount the first non-removable internal device on /media/hdd
+					DEVICETYPE="hdd"
 				else
-					readlink -fn /sys/block/$DEVBASE/device | grep -qs 'pci\|ahci\|sata'
-					EXTERNAL=$?
-					if [ "${REMOVABLE}" -eq "0" -a $EXTERNAL -eq 0 ] ; then
-						# mount the first non-removable internal device on /media/hdd
-						DEVICETYPE="hdd"
+					MODEL=`cat /sys/block/$DEVBASE/device/model`
+					if [ "$MODEL" == "USB CF Reader   " ]; then
+						DEVICETYPE="cf"
+					elif [ "$MODEL" == "Compact Flash   " ]; then
+						DEVICETYPE="cf"
+					elif [ "$MODEL" == "USB SD Reader   " ]; then
+						DEVICETYPE="mmc1"
+					elif [ "$MODEL" == "USB SD  Reader  " ]; then
+						DEVICETYPE="mmc1"
+					elif [ "$MODEL" == "SD/MMC          " ]; then
+						DEVICETYPE="mmc1"
+					elif [ "$MODEL" == "USB MS Reader   " ]; then
+						DEVICETYPE="mmc1"
+					elif [ "$MODEL" == "SM/xD-Picture   " ]; then
+						DEVICETYPE="mmc1"
+					elif [ "$MODEL" == "USB SM Reader   " ]; then
+						DEVICETYPE="mmc1"
+					elif [ "$MODEL" == "MS/MS-Pro       " ]; then
+						DEVICETYPE="mmc1"
+					elif [ "$MDEV" == "mmcblk0p1" ]; then
+						DEVICETYPE="mmc1"
 					else
-						MODEL=`cat /sys/block/$DEVBASE/device/model`
-						if [ "$MODEL" == "USB CF Reader   " ]; then
-							DEVICETYPE="cf"
-						elif [ "$MODEL" == "Compact Flash   " ]; then
-							DEVICETYPE="cf"
-						elif [ "$MODEL" == "USB SD Reader   " ]; then
-							DEVICETYPE="mmc1"
-						elif [ "$MODEL" == "USB SD  Reader  " ]; then
-							DEVICETYPE="mmc1"
-						elif [ "$MODEL" == "SD/MMC          " ]; then
-							DEVICETYPE="mmc1"
-						elif [ "$MODEL" == "USB MS Reader   " ]; then
-							DEVICETYPE="mmc1"
-						elif [ "$MODEL" == "SM/xD-Picture   " ]; then
-							DEVICETYPE="mmc1"
-						elif [ "$MODEL" == "USB SM Reader   " ]; then
-							DEVICETYPE="mmc1"
-						elif [ "$MODEL" == "MS/MS-Pro       " ]; then
-							DEVICETYPE="mmc1"
-						elif [ "$MDEV" == "mmcblk0p1" ]; then
-							DEVICETYPE="mmc1"
-						else
-							DEVICETYPE="usb"
-						fi
+						DEVICETYPE="usb"
 					fi
 				fi
 			else
