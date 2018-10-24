7c45604193b6b072654f47900b78c3e7ca47b330
littlesat <littlesat99@yahoo.com>
2018-10-24 20:21:11 +0200

    Adapt autoinstall.sh script for autoinstall screen with progress bars

--- meta-openpli/recipes-openpli/enigma2/settings-autorestore/autoinstall.sh
+++ meta-openpli/recipes-openpli/enigma2/settings-autorestore/autoinstall.sh
@@ -1,58 +1,8 @@
 #! /bin/sh
-# This script is run once when your box boots for the first time.
-# It installs the things from /hdd/backup/autoinstall
-# or from wherever the settings were restored
-
-BACKUPDIR=/media/hdd
-LOGFILE=/home/root/autoinstall.log
-MACADDR=`cat /sys/class/net/eth0/address | cut -b 1,2,4,5,7,8,10,11,13,14,16,17`
-
-if [ -f /tmp/backupdir ]
-then
-    BACKUPDIR=`cat /tmp/backupdir`
-else
-	for candidate in `cut -d ' ' -f 2 /proc/mounts | grep '^/media/'`
-	do
-		if [ -d ${candidate}/backup ]
-		then
-			if [ ! -f ${BACKUPDIR}/backup/.timestamp ]
-			then
-				BACKUPDIR=${candidate}
-			elif [ -f ${BACKUPDIR}/backup/autoinstall${MACADDR} ]
-			then
-				break
-			elif [ ${candidate}/backup/.timestamp -nt ${BACKUPDIR}/backup/.timestamp ]
-			then
-				BACKUPDIR=${candidate}
-			fi
-		fi
-	done
-fi
-
-if [ -f ${BACKUPDIR}/backup/autoinstall${MACADDR} ]
-then
-	AUTOINSTALL=${BACKUPDIR}/backup/autoinstall${MACADDR}
-else
-	AUTOINSTALL=${BACKUPDIR}/backup/autoinstall
-fi
-
 IPKG=/usr/bin/opkg
-
-# when available, bind the console during autoinstall
-[ -f /sys/class/vtconsole/vtcon1/bind ] && echo 1 > /sys/class/vtconsole/vtcon1/bind
-
-if [ -f ${AUTOINSTALL} ]
+if ! $IPKG list_installed | grep -q $1
 then
-	${IPKG} update 2>&1 | tee ${LOGFILE}
-	sed 's/,/ /g' $AUTOINSTALL | while read packagefile packageoption ; do
-		if ! $IPKG list_installed | grep -q $packagefile
-		then
-			$IPKG install ${packageoption} $packagefile
-		else
-			echo skipped already installed package $packagefile
-		fi
-	done 2>&1 | tee -a ${LOGFILE}
+	$IPKG install ${packageoption} $1
+else
+	echo skipped already installed package $1
 fi
-
-# done, unbind the console
-[ -f /sys/class/vtconsole/vtcon1/bind ] && echo 0 > /sys/class/vtconsole/vtcon1/bind
