198c4bf63f8866e43fbffb2d3681bf2e86665ea5
kueken <ohnemailadresse@arcor.de>
2022-05-26 23:57:46 +0200

    mmc-dev-mapping update script

--- meta-openpli/recipes-support/partitions-by-name/files/mmc-dev-mapping.sh
+++ meta-openpli/recipes-support/partitions-by-name/files/mmc-dev-mapping.sh
@@ -3,3 +3,8 @@
 [ ! -e "/dev/block/by-name" ] && mkdir -p /dev/block/by-name
 ln -sf ${DEVNAME} /dev/block/by-name/${PARTNAME}
 
+if [ -e "/dev/block/by-name/flag" ]; then
+    if [ ! -e /dev/block/by-name/bootoptions ]; then
+      ln -sf /boot/ /dev/block/by-name/bootoptions
+    fi
+fi
