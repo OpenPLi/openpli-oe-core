7c7901744160bbff0ed6071edc2885cc951ff98e
WanWizard <wanwizard@wanwizard.eu>
2022-09-17 19:05:04 +0100

    corrected typo

--- meta-openpli/recipes-openpli/enigma2/settings-autorestore/settings-restore.sh
+++ meta-openpli/recipes-openpli/enigma2/settings-autorestore/settings-restore.sh
@@ -203,7 +203,7 @@ if [[ -d /etc/cron/crontabs && ! -L /etc/cron ]]; then
 	# move them to /var/spool/cron/crobtabs
 	cd /etc/cron/crontabs
 	for file in *; do
-		mv $file /var/spool/cron/crobtabs/$file
+		mv $file /var/spool/cron/crontabs/$file
 	done
 	cd /
 	rm -rf /etc/cron
